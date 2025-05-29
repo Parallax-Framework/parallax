bfd385c262d9180e9217405ee281d69ca14d0b05
bloodycop <eon.bloodycop@gmail.com>
2025-05-30 01:57:18 +0300

    Update sv_chat.lua

--- gamemode/core/libraries/server/sv_chat.lua
+++ gamemode/core/libraries/server/sv_chat.lua
@@ -2,7 +2,7 @@
 -- @module ax.chat
 
 function ax.chat:SendSpeaker(speaker, uniqueID, text)
-    local players = {speaker}
+    local players = {}
     for k, v in player.Iterator() do
         if ( !IsValid(v) or !v:Alive() or !v:GetCharacter() ) then continue end
 
