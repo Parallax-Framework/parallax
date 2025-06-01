cf883d94336956e81a095650ca9915605ece190d
bloodycop <eon.bloodycop@gmail.com>
2025-06-01 04:29:28 +0300

    Precache

--- gamemode/core/libraries/shared/sh_faction.lua
+++ gamemode/core/libraries/shared/sh_faction.lua
@@ -2,21 +2,21 @@
 -- @module ax.faction
 
 local DEFAULT_MODELS = {
-    "models/player/group01/female_01.mdl",
-    "models/player/group01/female_02.mdl",
-    "models/player/group01/female_03.mdl",
-    "models/player/group01/female_04.mdl",
-    "models/player/group01/female_05.mdl",
-    "models/player/group01/female_06.mdl",
-    "models/player/group01/male_01.mdl",
-    "models/player/group01/male_02.mdl",
-    "models/player/group01/male_03.mdl",
-    "models/player/group01/male_04.mdl",
-    "models/player/group01/male_05.mdl",
-    "models/player/group01/male_06.mdl",
-    "models/player/group01/male_07.mdl",
-    "models/player/group01/male_08.mdl",
-    "models/player/group01/male_09.mdl"
+    Model("models/player/group01/female_01.mdl"),
+    Model("models/player/group01/female_02.mdl"),
+    Model("models/player/group01/female_03.mdl"),
+    Model("models/player/group01/female_04.mdl"),
+    Model("models/player/group01/female_05.mdl"),
+    Model("models/player/group01/female_06.mdl"),
+    Model("models/player/group01/male_01.mdl"),
+    Model("models/player/group01/male_02.mdl"),
+    Model("models/player/group01/male_03.mdl"),
+    Model("models/player/group01/male_04.mdl"),
+    Model("models/player/group01/male_05.mdl"),
+    Model("models/player/group01/male_06.mdl"),
+    Model("models/player/group01/male_07.mdl"),
+    Model("models/player/group01/male_08.mdl"),
+    Model("models/player/group01/male_09.mdl")
 }
 
 ax.faction = ax.faction or {}
