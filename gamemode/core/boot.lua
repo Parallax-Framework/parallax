b3fe21be7f5c63f4d267a88f153b3e2cf94fc7b7
bloodycop <eon.bloodycop@gmail.com>
2025-06-02 17:17:11 +0300

    Update boot.lua

--- gamemode/core/boot.lua
+++ gamemode/core/boot.lua
@@ -1,8 +1,28 @@
 GM.Name = "Parallax"
 GM.Author = "Riggs"
 GM.Description = "Parallax is a modular roleplay framework for Garry's Mod, built for performance, structure, and developer clarity."
-GM.Version = "0.0.1"
+GM.Version = "1.4.5"
 GM.ProductionState = "Alpha"
+
+local colorSuccess = Color(120, 255, 120)
+local colorWarning = Color(255, 200, 120)
+
+local versionFetchHttp = "https://raw.githubusercontent.com/bloodycop6385/parallax/refs/heads/main/version.txt"
+http.Fetch(versionFetchHttp, function(body)
+    if ( GAMEMODE.Version != body ) then
+        ax.util:PrintWarning("You are running an outdated version of Parallax! Please update to the latest version: " .. body)
+
+        for k, v in player.Iterator() do
+            v:ChatText(colorWarning, "You are running an outdated version of Parallax! Please update to the latest version: ", body)
+        end
+    else
+        ax.util:PrintSuccess("Parallax is up to date.")
+
+        for k, v in player.Iterator() do
+            v:ChatText(colorSuccess, "Parallax is up to date. Version: ", body)
+        end
+    end
+end)
 
 ax.util:Print("Framework Initializing...")
 ax.util:LoadFolder("libraries/external")
