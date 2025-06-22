02fa1e71008db2fb0a8f3639e9273d4daca26c08
Riggs <riggs9162@gmx.de>
2025-06-22 23:46:06 +0200

    Add inventory meta methods and properties
    Introduced default properties and utility methods to the INVENTORY meta table, including getters for ID, character, max weight, items, and a __tostring metamethod. Also added logic to calculate total inventory weight and handle invalid items.

--- gamemode/core/meta/sh_inventory.lua
+++ gamemode/core/meta/sh_inventory.lua
@@ -10,5 +10,47 @@
 ]]
 
 local INVENTORY = ax.inventory.meta or {}
+INVENTORY.Data = INVENTORY.Data or {}
+INVENTORY.ID = INVENTORY.ID or 0
+INVENTORY.Items = INVENTORY.Items or {}
+INVENTORY.MaxWeight = INVENTORY.MaxWeight or 0
+INVENTORY.CharacterID = INVENTORY.CharacterID or 0
+
+function INVENTORY:__tostring()
+    return "inventory[" .. self:GetID() .. "]"
+end
+
+function INVENTORY:GetID()
+    return self.ID
+end
+
+function INVENTORY:GetCharacterID()
+    return self.CharacterID
+end
+
+function INVENTORY:GetCharacter()
+    return ax.character:Get(self.CharacterID)
+end
+
+function INVENTORY:GetMaxWeight()
+    return self.MaxWeight
+end
+
+function INVENTORY:GetWeight()
+    local weight = 0
+    for _, item in ipairs(self:GetItems()) do
+        if ( item ) then
+            weight = weight + item:GetWeight()
+        else
+            ax.util:PrintWarning("Invalid item found in inventory " .. self:GetID() .. ": " .. tostring(item))
+        end
+    end
+
+    return weight
+end
+
+function INVENTORY:GetItems()
+    return self.Items
+end
 
 ax.inventory.meta = INVENTORY
