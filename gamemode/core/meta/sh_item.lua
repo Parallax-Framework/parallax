bfb94845d750e86e0a7d137f0a1cc2bd398ed28a
Riggs <riggs9162@gmx.de>
2025-06-22 23:07:43 +0200

    Update sh_item.lua

--- gamemode/core/meta/sh_item.lua
+++ gamemode/core/meta/sh_item.lua
@@ -11,7 +11,6 @@
 
 local ITEM = ax.item.meta or {}
 ITEM.Category = ITEM.Category or "Miscellaneous"
-ITEM.CharacterID = ITEM.CharacterID or 0
 ITEM.Data = ITEM.Data or {}
 ITEM.Description = ITEM.Description or "An item that is undefined."
 ITEM.Entity = ITEM.Entity or NULL
@@ -42,10 +41,6 @@ end
 
 function ITEM:GetInventoryID()
     return self.InventoryID
-end
-
-function ITEM:GetCharacterID()
-    return self.CharacterID
 end
 
 function ITEM:GetName()
@@ -94,42 +89,4 @@ end
 
 function ITEM:GetInventory()
     return ax.inventory.instances[self.InventoryID]
-end
-
-function ITEM:GetCharacter()
-    return ax.character.instances[self.CharacterID]
-end
-
-ITEM.Name = ITEM.Name or "Unnamed Item"
-ITEM.Description = ITEM.Description or "No description provided."
-ITEM.Model = ITEM.Model or Model("models/props_c17/oildrum001.mdl")
-ITEM.Weight = ITEM.Weight or 1
-ITEM.Price = ITEM.Price or 0
-ITEM.MaxStack = ITEM.MaxStack or 1
-ITEM.Category = ITEM.Category or "Miscellaneous"
--- We will not be having OwnerIDs, that'll be for inventories.
-
-ITEM.Actions = ITEM.Actions or {}
-
-function ITEM:AddAction(name, callback)
-    if ( !isnumber(name) and !isfunction(callback) ) then
-        ax.util:PrintError("Invalid parameters for ITEM:AddAction")
-        return
-    end
-
-    if ( self.Actions[name] ) then
-        ax.util:PrintError("Action with name \"" .. name .. "\" already exists.")
-        return
-    end
-
-    self.Actions[name] = callback
-end
-
-function ITEM:RemoveAction(name)
-    if ( !isfunction(self.Actions[name]) ) then
-        ax.util:PrintError("Action with name \"" .. name .. "\" does not exist.")
-        return
-    end
-
-    self.Actions[name] = nil
 end
