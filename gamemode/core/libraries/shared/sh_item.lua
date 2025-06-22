489cbb7c4b8ffb599de781980c136af194a6f86a
Riggs <riggs9162@gmx.de>
2025-06-23 00:46:13 +0200

    Update sh_item.lua

--- gamemode/core/libraries/shared/sh_item.lua
+++ gamemode/core/libraries/shared/sh_item.lua
@@ -18,72 +18,50 @@ function ax.item:Instance()
     return setmetatable({}, self.meta)
 end
 
+function ax.item:Get(input)
+    if ( !input ) then
+        ax.util:PrintError("Invalid unique ID provided to ax.item:Get()")
+        return false
+    end
+
+    if ( isstring(input) ) then -- Search through the stored items by unique ID
+        local item = self.stored[input]
+        if ( !item ) then
+            ax.util:PrintError("Item with unique ID '" .. input .. "' not found.")
+            return false
+        end
+
+        return item
+    elseif ( isnumber(input) ) then -- Search through the instances by ID
+        local item = self.instances[input]
+        if ( !item ) then
+            ax.util:PrintError("Item with ID '" .. input .. "' not found.")
+            return false
+        end
+
+        return item
+    end
+
+    ax.util:PrintError("Invalid input type provided to ax.item:Get()")
+    return false
+end
+
 function ax.item:Load(path)
-    --[[
     if ( !path or !isstring(path) ) then return end
 
     local files, _ = file.Find(path .. "/*.lua", "LUA")
     if ( !files or files[1] == nil ) then return end
 
+    print("Loading item files from: " .. path)
+    -- Recursively include all files in the folder.
     for i = 1, #files do
         local v = files[i]
-        local filePath = path .. "/" .. v
-        ITEM = setmetatable({}, self.meta)
-
-        ITEM.UniqueID = string.StripExtension(v):sub(4)
-
-        -- Check if we are in the /base/ folder, if so, we need to set the base table
-        -- to the ITEM table so we can use it in the item.
-        if ( string.find(filePath, "/base/") ) then
-            ITEM.IsBase = true
-            self.base[ITEM.UniqueID] = ITEM
+        if ( v:sub(-4) == ".lua" ) then
+            local filePath = path .. "/" .. v
+            ax.util:LoadFile(filePath, "shared")
+            print("Loaded item file: " .. filePath)
         end
-
-        -- If we are inside of a folder that is in the ax.item.base table, we need to set the base of the item to the base of the folder.
-        -- This allows us to inherit from the base item.
-        for k, _ in pairs(self.base) do
-            if ( string.find(path, "/" .. k) and !ITEM.Base and !ITEM.IsBase ) then
-                ITEM.Base = k
-                break
-            end
-        end
-
-        local bResult = hook.Run("PreItemRegistered", ITEM.UniqueID, ITEM)
-        if ( bResult == false ) then continue end
-
-        ITEM:AddDefaultActions()
-
-        -- Inherit the info from the base and add it to the item table.
-        if ( ITEM.Base ) then
-            local baseTable = self.base[ITEM.Base]
-            if ( baseTable ) then
-                for k2, v2 in pairs(baseTable) do
-                    if ( ITEM[k2] == nil ) then
-                        ITEM[k2] = v2
-                    end
-
-                    ITEM.BaseTable = baseTable
-                end
-
-                local mergeTable = table.Copy(baseTable)
-                ITEM = table.Merge(mergeTable, ITEM)
-            else
-                ax.util:PrintError("Item base '" .. ITEM.Base .. "' not found for item '" .. ITEM.UniqueID .. "'.")
-            end
-        end
-
-        ax.util:LoadFile(filePath, "shared")
-
-        self.stored[ITEM.UniqueID] = ITEM
-
-        if ( isfunction(ITEM.OnRegistered) ) then
-            ITEM:OnRegistered()
-        end
-
-        hook.Run("PostItemRegistered", ITEM.UniqueID, ITEM)
-        ITEM = nil
     end
-    ]]
 end
 
 function ax.item:LoadFolder(path)
