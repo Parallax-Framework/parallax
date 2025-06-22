9e795b3bef41466031706459e05dc47627ec487c
Riggs <riggs9162@gmx.de>
2025-06-22 23:06:49 +0200

    Move item meta methods from inventory to item module
    Relocated item property defaults and accessor methods from sh_inventory.lua to sh_item.lua. This centralizes item-related logic within the item meta module, improving code organization and maintainability.

--- gamemode/core/meta/sh_item.lua
+++ gamemode/core/meta/sh_item.lua
@@ -10,5 +10,94 @@
 ]]
 
 local ITEM = ax.item.meta or {}
+ITEM.Category = ITEM.Category or "Miscellaneous"
+ITEM.CharacterID = ITEM.CharacterID or 0
+ITEM.Data = ITEM.Data or {}
+ITEM.Description = ITEM.Description or "An item that is undefined."
+ITEM.Entity = ITEM.Entity or NULL
+ITEM.Hooks = ITEM.Hooks or {}
+ITEM.ID = ITEM.ID or 0
+ITEM.InventoryID = ITEM.InventoryID or 0
+ITEM.IsBase = ITEM.IsBase or false
+ITEM.Material = ITEM.Material or ""
+ITEM.Model = ITEM.Model or "models/props_c17/oildrum001.mdl"
+ITEM.Name = "Undefined"
+ITEM.Skin = ITEM.Skin or 0
+ITEM.UniqueID = ITEM.UniqueID or "undefined"
+ITEM.Weight = ITEM.Weight or 0
+
+--- Returns the unique identifier of the item.
+-- @return string The unique identifier of the item
+function ITEM:__tostring()
+    return "item[" .. self:GetUniqueID() .. "][" .. self:GetID() .. "]"
+end
+
+function ITEM:GetUniqueID()
+    return self.UniqueID
+end
+
+function ITEM:GetID()
+    return self.ID
+end
+
+function ITEM:GetInventoryID()
+    return self.InventoryID
+end
+
+function ITEM:GetCharacterID()
+    return self.CharacterID
+end
+
+function ITEM:GetName()
+    return self.Name
+end
+
+function ITEM:GetDescription()
+    return self.Description
+end
+
+function ITEM:GetModel()
+    return self.Model
+end
+
+function ITEM:GetMaterial()
+    return self.Material
+end
+
+function ITEM:GetSkin()
+    return self.Skin
+end
+
+function ITEM:GetWeight()
+    return self.Weight
+end
+
+function ITEM:GetCategory()
+    return self.Category
+end
+
+function ITEM:GetData()
+    return self.Data
+end
+
+function ITEM:GetEntity()
+    return self.Entity
+end
+
+function ITEM:IsBase()
+    return self.IsBase
+end
+
+function ITEM:GetHooks()
+    return self.Hooks
+end
+
+function ITEM:GetInventory()
+    return ax.inventory.instances[self.InventoryID]
+end
+
+function ITEM:GetCharacter()
+    return ax.character.instances[self.CharacterID]
+end
 
 ax.item.meta = ITEM
