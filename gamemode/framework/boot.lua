2061f39c7e0ad65984f11fec4983f5703b7c7740
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-20 00:35:35 +0300

    Add missing directory includes in boot.lua

--- gamemode/framework/boot.lua
+++ gamemode/framework/boot.lua
@@ -4,3 +4,7 @@ GM.Website = "https://project-ordinance.com/parallax/"
 GM.Email = "<email@example.com>"
 
 ax.util:IncludeDirectory("libraries")
+ax.util:IncludeDirectory("meta")
+ax.util:IncludeDirectory("hooks")
+ax.util:IncludeDirectory("networking")
+ax.util:IncludeDirectory("interface")
