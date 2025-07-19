3e2ef800b1fc58da58c6444eb63582324821ab6a
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-20 00:36:08 +0300

    Add ax.module initialization and loading functionality

---
+++ gamemode/framework/libraries/sh_module.lua
@@ -0,0 +1,49 @@
+ax.module = ax.module or {}
+ax.module.stored = ax.module.stored or {}
+
+--[[
+    for k, v in pairs(MODULE) do
+        if ( isfunction(v) ) then
+            AX_HOOKS_CACHE[k] = AX_HOOKS_CACHE[k] or {}
+            AX_HOOKS_CACHE[k][MODULE] = v
+        end
+    end
+]]
+
+-- bloodycop6385 :: Wanted to use the below code, but I started to re-consider due to schema support.
+--[[
+function ax.module:Initialise()
+    local files, directories = file.Find("gamemodes/" .. engine.ActiveGamemode() .. "/gamemode/modules/*.lua", "LUA")
+    if ( files[1] != nil ) then
+        for i = 1, #files do
+            local fileName = files[i]
+            local moduleName = string.StripExtension(fileName)
+
+            local prefix = string.sub(moduleName, 1, 3)
+            if ( prefix == "sh_" or prefix == "cl_" or prefix == "sv_" ) then
+                moduleName = string.sub(moduleName, 4)
+            end
+
+            MODULE = { uniqueID = moduleName }
+                ax.util:Include("gamemodes/" .. engine.ActiveGamemode() .. "/gamemode/modules/" .. fileName, "shared")
+            MODULE = nil
+        end
+    end
+
+    if ( directories[1] != nil ) then
+        for i = 1, #directories do
+            local directoryName = directories[i]
+            local moduleName = string.StripExtension(directoryName)
+
+            local prefix = string.sub(moduleName, 1, 3)
+            if ( prefix == "sh_" or prefix == "cl_" or prefix == "sv_" ) then
+                moduleName = string.sub(moduleName, 4)
+            end
+
+            MODULE = { uniqueID = moduleName }
+                ax.util:IncludeDirectory("gamemodes/" .. engine.ActiveGamemode() .. "/gamemode/modules/" .. directoryName)
+            MODULE = nil
+        end
+    end
+end
+]]
