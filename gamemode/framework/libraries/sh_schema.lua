248afcc4772283b7d0e7c2ab73e43dd707e9011b
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-07-20 00:36:13 +0300

    Fix path in schema boot file inclusion for active gamemode

--- gamemode/framework/libraries/sh_schema.lua
+++ gamemode/framework/libraries/sh_schema.lua
@@ -4,7 +4,7 @@ function ax.schema:Initialize()
     SCHEMA = SCHEMA or {}
 
     local active = engine.ActiveGamemode()
-    local boot = ax.util:Include(active .. "gamemode/schema/boot.lua", "shared")
+    local boot = ax.util:Include(active .. "/gamemode/schema/boot.lua", "shared")
     if ( !boot ) then
         ax.util:PrintError("Failed to load schema boot file for gamemode '" .. active .. "'")
         return false
