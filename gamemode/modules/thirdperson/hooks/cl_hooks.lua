0984d276ff1a329685451ed090665cc397a0fce2
Riggs <riggs9162@gmx.de>
2025-07-20 06:58:13 +0200

    Add ShouldDrawThirdPerson hook for third-person checks
    Introduces a ShouldDrawThirdPerson function to centralize logic for determining if third-person view should be enabled. Updates CalcView and ShouldDrawLocalPlayer to use this hook, improving code maintainability and consistency.

--- gamemode/modules/thirdperson/hooks/cl_hooks.lua
+++ gamemode/modules/thirdperson/hooks/cl_hooks.lua
@@ -1,4 +1,18 @@
+function MODULE:ShouldDrawThirdPerson(client)
+    if ( !IsValid(client) or !client:IsPlayer() ) then
+        return false
+    end
+
+    if ( !client:Alive() or client:InVehicle() or client:GetObserverMode() != OBS_MODE_NONE ) then
+        return false
+    end
+
+    return true
+end
+
 function MODULE:CalcView(client, origin, angles, fov)
+    if ( !hook.Run("ShouldDrawThirdPerson", client) ) then return end
+
     local view = {}
     view.origin = origin + angles:Forward() * -50 + angles:Right() * 25
     view.angles = angles
@@ -8,6 +22,8 @@ function MODULE:CalcView(client, origin, angles, fov)
 end
 
 function MODULE:ShouldDrawLocalPlayer(client)
+    if ( !hook.Run("ShouldDrawThirdPerson", client) ) then return end
+
     return true
 end
 
