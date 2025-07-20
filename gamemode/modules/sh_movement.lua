230798a70b9f91936d9a33893d6db0419a647501
Riggs <riggs9162@gmx.de>
2025-07-20 06:52:05 +0200

    Refactor OnPlayerHitGround to MODULE and add debug print
    Moved the OnPlayerHitGround function from GM to MODULE for better modularity. Added a debug print statement to log when a player's velocity is adjusted upon hitting the ground.

--- gamemode/modules/sh_movement.lua
+++ gamemode/modules/sh_movement.lua
@@ -1,11 +1,10 @@
-local MODULE = MODULE
-
 MODULE.Name = "Movement"
 MODULE.Description = "Restricts player movement to a specific set of rules."
 MODULE.Author = "Riggs"
 
 -- Anti-Bunnyhop
-function GM:OnPlayerHitGround(client, inWater)
+function MODULE:OnPlayerHitGround(client, inWater)
     local vel = client:GetVelocity()
     client:SetVelocity(Vector(-(vel.x / 2), - (vel.y / 2), 0))
+    print("Player " .. client:Name() .. " hit the ground and velocity was adjusted.")
 end
