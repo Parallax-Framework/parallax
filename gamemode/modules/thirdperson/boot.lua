d466123e6a12aa90f40a912be268642145c69c5b
riggs9162 <riggs9162@gmx.de>
2025-08-01 21:58:53 +0200

    Remove IncludeDirectory call for hooks in thirdperson module
    The ax.util:IncludeDirectory("hooks") line was removed from the thirdperson module's boot.lua, possibly to prevent automatic inclusion of the hooks directory or because it is no longer needed.

--- gamemode/modules/thirdperson/boot.lua
+++ gamemode/modules/thirdperson/boot.lua
@@ -4,6 +4,4 @@ MODULE.Name = "Third Person"
 MODULE.Description = "Adds third-person camera functionality to the gamemode."
 MODULE.Author = "Riggs"
 
-ax.util:IncludeDirectory("hooks")
-
 print("Initializing module: " .. MODULE.Name)
