7ff65d19e4c7548c08196d0a151ada3714b32ce6
riggs9162 <riggs9162@gmx.de>
2025-08-18 19:57:04 +0200

    Expand animation model support for genders
    Updated the animation model lists for male and female genders in sh_wiltostech.lua to include additional gesture and posture models. This enhances compatibility and variety for character animations by registering more shared and specific models.

--- gamemode/framework/libraries/thirdparty/sh_wiltostech.lua
+++ gamemode/framework/libraries/thirdparty/sh_wiltostech.lua
@@ -4,22 +4,38 @@ hook.Add("InitLoadAnimations", "wOS.DynaBase.CustomMount", function()
     wOS.DynaBase:RegisterSource({
         Name = "Parallax",
         Type = WOS_DYNABASE.REANIMATION,
-        Male = "models/humans/male_gestures.mdl",
-        Female = "models/humans/female_gestures.mdl"
+        Male = "models/humans/male_shared.mdl",
+        Female = "models/humans/female_shared.mdl"
     })
 
     hook.Add("PreLoadAnimations", "wOS.DynaBase.MountCustom", function(gender)
         if ( gender == WOS_DYNABASE.SHARED ) then return end
 
         if ( gender == WOS_DYNABASE.FEMALE ) then
-            IncludeModel("models/mossman_gestures.mdl")
+            IncludeModel("models/alyx_animations.mdl")
             IncludeModel("models/alyx_gest_ep1.mdl")
             IncludeModel("models/alyx_gest_ep2.mdl")
-            IncludeModel("models/Eli_gestures.mdl")
-            IncludeModel("models/humans/male_gestures.mdl")
+            IncludeModel("models/alyx_gestures.mdl")
+            IncludeModel("models/alyx_postures.mdl")
+            IncludeModel("models/eli_gestures.mdl")
+            IncludeModel("models/eli_postures.mdl")
+            IncludeModel("models/mossman_gestures.mdl")
+            IncludeModel("models/mossman_postures.mdl")
+            IncludeModel("models/humans/female_shared.mdl")
+            IncludeModel("models/humans/female_ss.mdl")
+            IncludeModel("models/humans/female_gestures.mdl")
+            IncludeModel("models/humans/female_postures.mdl")
         elseif ( gender == WOS_DYNABASE.MALE ) then
-            IncludeModel("models/Eli_gestures.mdl")
+            IncludeModel("models/barney_gestures.mdl")
+            IncludeModel("models/barney_postures.mdl")
+            IncludeModel("models/combine_soldier_anims.mdl")
+            IncludeModel("models/eli_gestures.mdl")
+            IncludeModel("models/eli_postures.mdl")
+            IncludeModel("models/police_ss.mdl")
+            IncludeModel("models/humans/male_shared.mdl")
+            IncludeModel("models/humans/male_ss.mdl")
             IncludeModel("models/humans/male_gestures.mdl")
+            IncludeModel("models/humans/male_postures.mdl")
         end
     end)
 end)
