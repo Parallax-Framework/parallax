e94afbea95de471b66a5b86ff93568cbae047ba8
riggs9162 <riggs9162@gmx.de>
2025-09-14 19:03:19 +0200

    feat: Enhance inventory panel with item actions and dynamic info display

--- gamemode/framework/interface/cl_inventory.lua
+++ gamemode/framework/interface/cl_inventory.lua
@@ -22,7 +22,7 @@ function PANEL:Init()
     self.info = self:Add("EditablePanel")
     self.info:Dock(RIGHT)
     self.info:DockPadding(ScreenScale(4), ScreenScaleH(4), ScreenScale(4), ScreenScaleH(4))
-    self.info:SetWide(0) -- ScreenScale(128)
+    self.info:SetWide(0)
     self.info.Paint = function(this, width, height)
         draw.RoundedBox(0, 0, 0, width, height, Color(0, 0, 0, 150))
     end
@@ -59,12 +59,85 @@ function PANEL:Init()
         item:SetContentAlignment(4)
         item:SetTextInset(item:GetTall() + ScreenScale(2), 0)
 
+        item.DoClick = function()
+            self.info:Motion(0.25, {
+                Target = {wide = ScreenScale(196), fraction = 1.0},
+                Easing = "OutQuad",
+                Think = function(this)
+                    self.info:SetWide(this.wide)
+                    self.info:DockMargin(ScreenScale(16) * this.fraction, 0, 0, 0)
+                end
+            })
+
+            self.info:Clear()
+
+            self:PopulateInfo(v)
+        end
+
         local icon = item:Add("SpawnIcon")
         icon:Dock(LEFT)
         icon:SetWide(item:GetTall())
         icon:DockMargin(0, 0, ScreenScale(4), 0)
         icon:SetModel(v:GetModel() or "models/props_junk/wood_crate001a.mdl")
         icon:SetMouseInputEnabled(false)
+    end
+end
+
+function PANEL:PopulateInfo(item)
+    if ( !istable(item) ) then return end
+
+    local title = self.info:Add("ax.text")
+    title:Dock(TOP)
+    title:SetFont("ax.large.bold")
+    title:SetText(item:GetName() or "Unknown Item")
+    title:SizeToContentsY()
+    title:DockMargin(0, 0, 0, ScreenScaleH(4))
+    title:SetContentAlignment(5)
+
+    local description = self.info:Add("ax.text")
+    description:Dock(TOP)
+    description:SetFont("ax.regular")
+    description:SetText(item:GetDescription() or "No description available.")
+    description:SizeToContentsY()
+    description:DockMargin(0, 0, 0, ScreenScaleH(8))
+    description:SetContentAlignment(5)
+
+    local actions = item:GetActions()
+    if ( #actions < 1 ) then
+        local noActions = self.info:Add("ax.text")
+        noActions:Dock(TOP)
+        noActions:SetFont("ax.regular.italic")
+        noActions:SetText("No actions available for this item.")
+        noActions:SizeToContentsY()
+        noActions:DockMargin(0, 0, 0, ScreenScaleH(4))
+        noActions:SetContentAlignment(5)
+
+        return
+    end
+
+    for k, v in pairs(actions) do
+        local actionButton = self.info:Add("ax.button.flat")
+        actionButton:Dock(TOP)
+        actionButton:SetFont("ax.regular")
+        actionButton:SetText(v.name or k)
+        actionButton:DockMargin(0, 0, 0, ScreenScaleH(4))
+        actionButton:SetContentAlignment(5)
+
+        actionButton.DoClick = function()
+            net.Start("ax.inventory.item.action")
+            net.WriteUInt(item.id, 32)
+            net.WriteString(k)
+            net.SendToServer()
+
+            self.info:Motion(0.25, {
+                Target = {wide = 0.0, fraction = 0.0},
+                Easing = "OutQuad",
+                Think = function(this)
+                    self.info:SetWide(this.wide)
+                    self.info:DockMargin(ScreenScale(16) * this.fraction, 0, 0, 0)
+                end
+            })
+        end
     end
 end
 
