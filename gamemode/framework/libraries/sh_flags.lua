1b0a14af9eee29cb316a52e2dd63cbc03f5a06a5
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-09-19 01:02:21 +0300

    feat(flags): Library

---
+++ gamemode/framework/libraries/sh_flags.lua
@@ -0,0 +1,24 @@
+ax.flag = ax.flag or {}
+ax.flag.stored = ax.flag.stored or {}
+
+function ax.flag:Create( letter, flagData )
+    if ( !isstring(letter) or #letter > 1 ) then
+        ax.util:PrintError("Invalid flag letter provided to ax.flag:Create()")
+        return
+    end
+
+    self.stored[letter] = flagData
+end
+
+function ax.flag:GetAll()
+    return self.stored
+end
+
+function ax.flag:Get(letter)
+    if ( !isstring(letter) or #letter > 1 ) then
+        ax.util:PrintError("Invalid flag letter provided to ax.flag:Get()")
+        return
+    end
+
+    return self.stored[letter]
+end
