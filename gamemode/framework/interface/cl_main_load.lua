303fc8f697564a4ed25ebc1c68155caf31c8963a
riggs9162 <riggs9162@gmx.de>
2025-09-18 22:55:44 +0200

    feat(ui): Implement character deletion panel and functionality

--- gamemode/framework/interface/cl_main_load.lua
+++ gamemode/framework/interface/cl_main_load.lua
@@ -1,8 +1,8 @@
 local PANEL = {}
 
 --[[
-net.Start("ax.character.load")
-    net.WriteUInt(1, 32)
+net.Start("ax.character.delete")
+    net.WriteUInt(v.id, 32)
 net.SendToServer()
 ]]
 
@@ -12,20 +12,34 @@ function PANEL:Init()
     self:SetPos(0, 0)
     self:SetSize(ScrW(), ScrH())
 
-    self:CreateNavigation(self, "back", function()
+    self.characterList = self:Add("ax.transition")
+    self.characterList:StartAtBottom()
+
+    self:CreateNavigation(self.characterList, "back", function()
         self:SlideDown()
         parent.splash:SlideToFront()
     end)
 
-    self.characters = self:Add("ax.scroller.vertical")
+    self.characters = self.characterList:Add("ax.scroller.vertical")
     self.characters:Dock(FILL)
     self.characters:DockMargin(ScreenScale(128), ScreenScaleH(32), ScreenScale(128), ScreenScaleH(32))
     self.characters:InvalidateParent(true)
     self.characters:GetVBar():SetWide(0)
     self.characters.Paint = nil
+
+    self.deletePanel = self:Add("ax.transition")
+    self.deletePanel:StartAtRight()
+
+    self.deleteContainer = self.deletePanel:Add("EditablePanel")
+    self.deleteContainer:Dock(FILL)
+    self.deleteContainer:InvalidateParent(true)
 end
 
 function PANEL:OnSlideStart()
+    if ( !IsValid(self.characters) ) then return end
+    if ( !IsValid(self.deleteContainer) ) then return end
+
+    self.characterList:SlideToFront()
     self:PopulateCharacterList()
 end
 
@@ -91,11 +105,7 @@ function PANEL:PopulateCharacterList()
         deleteButton:SetContentAlignment(5)
         deleteButton.width = 0
         deleteButton.DoClick = function()
-            -- self:PopulateDelete(v.id)
-
-            net.Start("ax.character.delete")
-                net.WriteUInt(v.id, 32)
-            net.SendToServer()
+            self:PopulateDeletePanel(v)
         end
 
         -- Sorry for this pyramid of code, but eon wanted me to make the delete button extend when hovered over the character button.
@@ -143,6 +153,59 @@ function PANEL:PopulateCharacterList()
             this:SetTextColor(button:GetTextColor())
         end
     end
+end
+
+function PANEL:PopulateDeletePanel(character)
+    self.characterList:SlideLeft()
+    self.deletePanel:SlideToFront()
+    self.deleteContainer:Clear()
+
+    local title = self.deleteContainer:Add("ax.text")
+    title:Dock(TOP)
+    title:DockMargin(ScreenScale(32), ScreenScaleH(32), 0, 0)
+    title:SetFont("ax.huge.bold")
+    title:SetText("ARE YOU SURE YOU WANT TO DELETE")
+
+    local name = self.deleteContainer:Add("ax.text")
+    name:Dock(TOP)
+    name:DockMargin(ScreenScale(64), 0, 0, ScreenScaleH(16))
+    name:SetFont("ax.huge.bold")
+    name:SetText(character:GetName():upper())
+    name:SetTextColor(team.GetColor(character:GetFaction()) or color_white)
+
+    local mark = name:Add("ax.text")
+    mark:Dock(LEFT)
+    mark:DockMargin(name:GetWide(), 0, 0, 0)
+    mark:SetFont("ax.huge.bold")
+    mark:SetText("?")
+
+    local model = self.deleteContainer:Add("DModelPanel")
+    model:SetModel(character:GetModel() or "models/player/kleiner.mdl")
+    model:SetFOV(15)
+    model:SetSize(self.deleteContainer:GetWide() / 3, self.deleteContainer:GetTall())
+    model:Center()
+    model:SetMouseInputEnabled(false)
+    model.LayoutEntity = function(this, ent)
+        ent:SetAngles(Angle(0, RealTime() * 10 % 360, 0))
+        ent:SetPos(-Vector(128, 128, 32))
+        ent:SetEyeTarget(ent:GetPos() * ent:GetAngles():Forward())
+        ent:SetIK(false)
+
+        this:RunAnimation()
+    end
+
+    self:CreateNavigation(self.deleteContainer, "back", function()
+        self.characterList:SlideToFront()
+        self.deletePanel:SlideRight()
+    end, "confirm", function()
+        net.Start("ax.character.delete")
+            net.WriteUInt(character.id, 32)
+        net.SendToServer()
+
+        self:PopulateCharacterList()
+        self.characterList:SlideToFront()
+        self.deletePanel:SlideRight()
+    end)
 end
 
 vgui.Register("ax.main.load", PANEL, "ax.transition")
