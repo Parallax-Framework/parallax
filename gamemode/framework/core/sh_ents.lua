71f066a789347f205d6e778986c60e82a428131f
riggs9162 <riggs9162@gmx.de>
2025-09-22 19:40:24 +0200

    fix(indent): Normalize Lua indentation and formatting

--- gamemode/framework/core/sh_ents.lua
+++ gamemode/framework/core/sh_ents.lua
@@ -10,14 +10,14 @@
 ]]
 
 function ents.FindInCube(center, radius)
-	if ( !isvector(center) ) then
-		return error("Invalid argument: 'center' must be a Vector")
-	end
+    if ( !isvector(center) ) then
+        return error("Invalid argument: 'center' must be a Vector")
+    end
 
-	if ( !isnumber(radius) or radius <= 0 ) then
-		return error("Invalid argument: 'radius' must be a positive number")
-	end
+    if ( !isnumber(radius) or radius <= 0 ) then
+        return error("Invalid argument: 'radius' must be a positive number")
+    end
 
-	local rvec = Vector(radius, radius, radius)
-	return ents.FindInBox(center - rvec, center + rvec)
+    local rvec = Vector(radius, radius, radius)
+    return ents.FindInBox(center - rvec, center + rvec)
 end
--- gamemode/modules/discord/config/sv_config.lua
+++ gamemode/modules/discord/config/sv_config.lua
@@ -1,20 +1,20 @@
 ax.discord.config = {
-	enabled = true,
+    enabled = true,
 
-	channels = {
-		public = {
-			webhook = "https://discord.com/api/webhooks/1234567890/TOKEN",
-			color = 0x2ecc71
-		},
-		private = {
-			webhook = "https://discord.com/api/webhooks/0987654321/TOKEN",
-			color = 0xe67e22
-		}
-	},
+    channels = {
+        public = {
+            webhook = "https://discord.com/api/webhooks/1234567890/TOKEN",
+            color = 0x2ecc71
+        },
+        private = {
+            webhook = "https://discord.com/api/webhooks/0987654321/TOKEN",
+            color = 0xe67e22
+        }
+    },
 
-	useWebSocket = false,
-	webSocketURL = "",
+    useWebSocket = false,
+    webSocketURL = "",
 
-	name = "Parallax",
-	avatarURL = "https://cdn.discordapp.com/icons/1361111176632860913/e73548badcc41c7496dcff8bff0a9d1c.webp?size=1024",
+    name = "Parallax",
+    avatarURL = "https://cdn.discordapp.com/icons/1361111176632860913/e73548badcc41c7496dcff8bff0a9d1c.webp?size=1024",
 }
