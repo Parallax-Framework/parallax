1f5e8b3aada6095d85e34a739b08ac13f8e75043
riggs9162 <riggs9162@gmx.de>
2025-09-23 23:27:58 +0200

    refactor(interface): Replace ax.tab.config and ax.tab.settings with ax.store in PopulateTabButtons

--- gamemode/framework/interface/cl_config.lua
+++ gamemode/framework/interface/cl_config.lua
@@ -1,18 +1,8 @@
-local PANEL = {}
-
-function PANEL:Init()
-    self:Dock(FILL)
-end
-
-function PANEL:Paint(width, height)
-end
-
-vgui.Register("ax.tab.config", PANEL, "DPanel")
-
 hook.Add("PopulateTabButtons", "ax.tab.config", function(buttons)
     buttons["config"] = {
         Populate = function(this, panel)
-            panel:Add("ax.tab.config")
+            local settings = panel:Add("ax.store")
+            settings:SetType("config")
         end
     }
 end)
--- gamemode/framework/interface/cl_settings.lua
+++ gamemode/framework/interface/cl_settings.lua
@@ -1,18 +1,8 @@
-local PANEL = {}
-
-function PANEL:Init()
-    self:Dock(FILL)
-end
-
-function PANEL:Paint(width, height)
-end
-
-vgui.Register("ax.tab.settings", PANEL, "DPanel")
-
 hook.Add("PopulateTabButtons", "ax.tab.settings", function(buttons)
     buttons["settings"] = {
         Populate = function(this, panel)
-            panel:Add("ax.tab.settings")
+            local settings = panel:Add("ax.store")
+            settings:SetType("option")
         end
     }
 end)
