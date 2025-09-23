ed22bd30da9182681af846e106e2530d52f774c6
riggs9162 <riggs9162@gmx.de>
2025-09-23 22:20:01 +0200

    feat(thirdperson): Add option to follow player's head movements in third-person view

--- gamemode/modules/cl_thirdperson.lua
+++ gamemode/modules/cl_thirdperson.lua
@@ -8,6 +8,8 @@ ax.option:Add("thirdperson", ax.type.bool, false, { category = "Camera" })
 ax.option:Add("thirdperson_x", ax.type.number, 25, { category = "Camera" })
 ax.option:Add("thirdperson_y", ax.type.number, 0, { category = "Camera" })
 ax.option:Add("thirdperson_z", ax.type.number, -50, { category = "Camera" })
+ax.option:Add("thirdperson_follow_head", ax.type.bool, true, { category = "Camera", description = "Make the third-person camera follow the player's model head movements." })
+
 local FIXED_RADIUS = 6
 
 -- LVS is gay and already used the ShouldDrawThirdPerson hook, so we have to use a different name
@@ -24,6 +26,14 @@ ax.viewstack:RegisterModifier("thirdperson", function(client, view)
 
     -- start from the player's eye position
     local startPos = client:EyePos()
+    local head = client:LookupBone("ValveBiped.Bip01_Head1")
+    if ( ax.option:Get("thirdperson_follow_head") and head ) then
+        local headPos, _ = client:GetBonePosition(head)
+        if ( headPos ) then
+            startPos = headPos
+        end
+    end
+
     local ang = view.angles
 
     -- desired camera offset relative to view angles
