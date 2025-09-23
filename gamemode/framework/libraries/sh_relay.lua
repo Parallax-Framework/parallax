16e455341b3147943de373ebaf9b06296d65cc3c
riggs9162 <riggs9162@gmx.de>
2025-09-23 21:31:22 +0200

    feat(relay): Add global relay functions for setting and getting relay data

--- gamemode/framework/libraries/sh_relay.lua
+++ gamemode/framework/libraries/sh_relay.lua
@@ -41,6 +41,36 @@ function ENTITY:GetRelay( name, fallback )
     return ax.relay.data[index][name] != nil and ax.relay.data[index][name] or fallback
 end
 
+-- Global variant, similatr to SetGlobalVar/GetGlobalVar
+function SetRelay( name, value, bNoNetworking, recipients )
+    if ( !isstring( name ) ) then
+        ax.util:PrintError("Invalid 'name' argument provided to function SetRelay()")
+        return
+    end
+
+    ax.relay.data["global"] = ax.relay.data["global"] or {}
+    ax.relay.data["global"][name] = value
+
+    if ( !bNoNetworking and SERVER ) then
+        net.Start( "ax.relay.update" )
+            net.WriteString( "global" )
+            net.WriteString( name )
+            net.WriteType( value )
+        if ( recipients ) then
+            net.Send( recipients )
+        else
+            net.Broadcast()
+        end
+    end
+end
+
+function GetRelay( name, fallback )
+    if ( !isstring( name ) ) then return fallback end
+
+    ax.relay.data["global"] = ax.relay.data["global"] or {}
+    return ax.relay.data["global"][name] != nil and ax.relay.data["global"][name] or fallback
+end
+
 hook.Add( "OnEntityRemoved", "ax.relay.cleanup", function( ent )
     local index = tostring( ent:EntIndex() )
     if ( ent:IsPlayer() ) then
