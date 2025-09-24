16399ce44144beec1ff9e88a4eb04f61b2ba7ab3
riggs9162 <riggs9162@gmx.de>
2025-09-24 17:16:37 +0200

    refactor(animations): Simplify animation update logic and improve player model handling

--- gamemode/modules/animations/hooks/sv_hooks.lua
+++ gamemode/modules/animations/hooks/sv_hooks.lua
@@ -1,28 +1,6 @@
 local MODULE = MODULE
 
-function MODULE:PostEntitySetModel(ent, model)
-    if ( !IsValid(ent) or !ent:IsPlayer() ) then return end
-
-    local client = ent
-    local clientTable = client:GetTable()
-    if ( !clientTable ) then return end
-
-    local holdType = client:GetHoldType()
-    local animTable = ax.animations.stored[ax.animations:GetModelClass(model)]
-    if ( animTable and animTable[holdType] ) then
-        clientTable.axAnimations = animTable[holdType]
-    else
-        clientTable.axAnimations = {}
-    end
-
-    net.Start("ax.animations.update")
-        net.WritePlayer(client)
-        net.WriteTable(clientTable.axAnimations)
-        net.WriteString(holdType)
-    net.Broadcast()
-end
-
-function MODULE:PlayerLoadout(client)
+local function UpdateClientAnimations(client)
     if ( !IsValid(client) ) then return end
 
     local clientTable = client:GetTable()
@@ -40,29 +18,33 @@ function MODULE:PlayerLoadout(client)
         net.WritePlayer(client)
         net.WriteTable(clientTable.axAnimations)
         net.WriteString(holdType)
-    net.Broadcast()
+    net.Send(client)
+end
+
+function MODULE:PostEntitySetModel(ent, model)
+    if ( !IsValid(ent) or !ent:IsPlayer() ) then return end
+
+    UpdateClientAnimations(ent)
+end
+
+function MODULE:PlayerLoadout(client)
+    if ( !IsValid(client) ) then return end
+
+    UpdateClientAnimations(client)
 end
 
 function MODULE:PlayerSwitchWeapon(client, oldWeapon, newWeapon)
     timer.Simple(0, function()
+        if ( !IsValid(client) or !IsValid(newWeapon) ) then return end
+        UpdateClientAnimations(client)
+    end)
+end
+
+function MODULE:PlayerNoClip(client, toggle)
+    if ( !IsValid(client) ) then return end
+
+    timer.Simple(0, function()
         if ( !IsValid(client) ) then return end
-        if ( !IsValid(newWeapon) ) then return end
-
-        local clientTable = client:GetTable()
-        if ( !clientTable ) then return end
-
-        local holdType = client:GetHoldType()
-        local animTable = ax.animations.stored[ax.animations:GetModelClass(client:GetModel())]
-        if ( animTable and animTable[holdType] ) then
-            clientTable.axAnimations = animTable[holdType]
-        else
-            clientTable.axAnimations = {}
-        end
-
-        net.Start("ax.animations.update")
-            net.WritePlayer(client)
-            net.WriteTable(clientTable.axAnimations)
-            net.WriteString(holdType)
-        net.Broadcast()
+        UpdateClientAnimations(client)
     end)
 end
