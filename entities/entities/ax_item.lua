dfab9baab72e7ae7036bb2a2e45178ab3f4ea832
riggs9162 <riggs9162@gmx.de>
2025-10-03 00:02:00 +0200

    feat(item): Add ItemClass network variable and enhance item usage logic

--- entities/entities/ax_item.lua
+++ entities/entities/ax_item.lua
@@ -8,6 +8,7 @@ ENT.Category = "Parallax"
 
 function ENT:SetupDataTables()
     self:NetworkVar("Int", 0, "ItemID")
+    self:NetworkVar("String", 0, "ItemClass")
 end
 
 function ENT:GetItemTable()
@@ -16,7 +17,10 @@ end
 
 if ( SERVER ) then
     function ENT:Initialize()
-        self:SetModel(Model("models/props_junk/gnome.mdl"))
+        local item = self:GetItemTable()
+        if ( !istable(item) ) then print("No item found!") return end
+
+        self:SetModel(item:GetModel() or Model("models/props_junk/gnome.mdl"))
         self:PhysicsInit(SOLID_VPHYSICS)
         self:SetSolid(SOLID_VPHYSICS)
         self:SetUseType(SIMPLE_USE)
@@ -25,5 +29,35 @@ if ( SERVER ) then
 
     function ENT:UpdateTransmitState()
         return TRANSMIT_PVS
+    end
+
+    function ENT:Use(activator, caller)
+        if ( !IsValid(activator) or !activator:IsPlayer() ) then return end
+
+        local item = self:GetItemTable()
+        if ( !istable(item) ) then print("No item found!") return end
+
+        local character = activator:GetCharacter()
+        if ( !istable(character) ) then print("No character found!") return end
+
+        local inventory = character:GetInventory()
+        if ( !istable(inventory) ) then print("No inventory found!") return end
+
+        if ( inventory:GetWeight() + item:GetWeight() <= inventory:GetMaxWeight() ) then
+            local success, reason = ax.item:Transfer(item, 0, inventory, function(success)
+                if ( success ) then
+                    activator:Notify("You have picked up: " .. (item:GetName() or "Unknown Item"))
+                    SafeRemoveEntity(self)
+                else
+                    activator:Notify("Failed to pick up item: " .. (item:GetName() or "Unknown Item"))
+                end
+            end)
+
+            if ( success == false ) then
+                activator:Notify(string.format("Failed to pick up item: %s", reason or "Unknown Reason"))
+            end
+        else
+            activator:Notify("You cannot carry any more of this item!")
+        end
     end
 end
