0bfedadf57bcc982d2ce5d5b0dccc3f824398d1d
riggs9162 <riggs9162@gmx.de>
2025-10-14 01:16:17 +0200

    feat(interface): add option tabs and padding to main options panel

--- gamemode/framework/interface/cl_main_options.lua
+++ gamemode/framework/interface/cl_main_options.lua
@@ -10,6 +10,14 @@ function PANEL:Init()
         self:SlideDown()
         parent.splash:SlideToFront()
     end)
+
+    local settings = self:Add("ax.store")
+    settings:SetType("option")
+    settings:DockMargin(ScreenScale(32), ScreenScaleH(32), ScreenScale(32), ScreenScaleH(32))
+
+    for _, tab in ipairs(settings:GetPages()) do
+        tab:DockPadding(0, 0, ScreenScale(64), 0)
+    end
 end
 
 vgui.Register("ax.main.options", PANEL, "ax.transition")
