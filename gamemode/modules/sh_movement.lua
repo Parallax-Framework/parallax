c9d228ec9a61e0636aa0711f7c16f0df8d4730f5
riggs9162 <riggs9162@gmx.de>
2025-10-14 22:12:43 +0200

    fix(movement): use config-driven bunnyhop velocity reduction

--- gamemode/modules/sh_movement.lua
+++ gamemode/modules/sh_movement.lua
@@ -4,8 +4,8 @@ MODULE.Name = "Movement"
 MODULE.Description = "Restricts player movement to a specific set of rules."
 MODULE.Author = "Riggs"
 
-local bunnyhopVelocityMultiplier = 0.5
 function MODULE:OnPlayerHitGround(client, inWater)
+    local bunnyhopVelocityMultiplier = ax.config:Get("movementBunnyhopReduction", 0.5)
     local velocity = client:GetVelocity()
     local horizontalVelocity = Vector(velocity.x, velocity.y, 0)
     local reducedVelocity = -horizontalVelocity * bunnyhopVelocityMultiplier
