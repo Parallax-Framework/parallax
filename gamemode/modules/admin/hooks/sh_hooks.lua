6ed754a61ef6417bd0ea607bab438ebe09903342
riggs9162 <riggs9162@gmx.de>
2025-10-19 00:15:54 +0200

    refactor(hooks): move PlayerNoClip function to sh_hooks.lua

---
+++ gamemode/modules/admin/hooks/sh_hooks.lua
@@ -0,0 +1,23 @@
+local MODULE = MODULE or {}
+
+function MODULE:PlayerNoClip(client, desiredState)
+    if ( !CAMI.PlayerHasAccess(client, "Parallax - Observer", nil) ) then
+        return false
+    end
+
+    if ( desiredState ) then
+        client:SetNoDraw(true)
+        client:SetNotSolid(true)
+        client:SetNoTarget(true)
+        client:DrawWorldModel(false)
+        client:DrawShadow(false)
+        client:GodEnable()
+    else
+        client:SetNoDraw(false)
+        client:SetNotSolid(false)
+        client:SetNoTarget(false)
+        client:DrawWorldModel(true)
+        client:DrawShadow(true)
+        client:GodDisable()
+    end
+end
