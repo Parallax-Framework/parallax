6d3514e864a575876aa2c9f978708266814862a3
riggs9162 <riggs9162@gmx.de>
2025-11-09 21:28:25 +0100

    feat(animations): improve ik control and descriptions

--- gamemode/modules/animations/boot.lua
+++ gamemode/modules/animations/boot.lua
@@ -4,14 +4,14 @@ MODULE.name = "Animations"
 MODULE.description = "Handles player animations."
 MODULE.author = "Riggs"
 
-ax.config:Add("animations.ik.enabled", ax.type.bool, true, {
+ax.config:Add("animations.ik", ax.type.bool, true, {
     category = "animations",
     subCategory = "general",
-    description = "animations.ik.enabled.help"
+    description = "animations.ik.help"
 })
 
 local LANG = {}
-LANG["animations.ik.enabled"] = "Enable Inverse Kinematics (IK) for player animations."
-LANG["animations.ik.enabled.help"] = "When enabled, the player's feet will adjust to uneven terrain for more realistic movement."
+LANG["config.animations.ik"] = "Inverse Kinematics (IK)"
+LANG["config.animations.ik.help"] = "When enabled, the player's feet will adjust to uneven terrain for more realistic movement."
 LANG["category.animations"] = "Animations"
 ax.localization:Register("en", LANG)
