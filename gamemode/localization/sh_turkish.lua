674596e5e615643e49b284c4b17c435785672784
wilderwesten <wilderwesten64@gmail.com>
2025-11-27 22:36:20 +0300

    sh_turkish.lua (2nd attempt) (#75)

---
+++ gamemode/localization/sh_turkish.lua
@@ -0,0 +1,116 @@
+ax.localization:Register("tr", {
+    -- Main Menu Translations
+    ["mainmenu.category.00_faction"] = "Fraksiyonlar",
+    ["mainmenu.category.01_appearance"] = "Görünüm",
+    ["mainmenu.create"] = "Karakter Oluştur",
+    ["mainmenu.disconnect"] = "Bağlantıyı Kes",
+    ["mainmenu.load"] = "Karakter Yükle",
+    ["mainmenu.options"] = "Seçenekler",
+    ["mainmenu.play"] = "Oyna",
+
+    -- Tab Menu Translations
+    ["tab.config"] = "Yapılandırma",
+    ["tab.help"] = "Yardım",
+    ["tab.inventory"] = "Envanter",
+    ["tab.scoreboard"] = "Skorbord",
+    ["tab.settings"] = "Seçenekler",
+
+    -- Category Translations
+    ["category.chat"] = "Sohbet",
+    ["category.chatbox"] = "Sohbet",
+    ["category.gameplay"] = "Oynanış",
+    ["category.general"] = "Genel",
+    ["category.interface"] = "Arayüz",
+    ["category.modules"] = "Modüller",
+
+    -- Subcategory Translations
+    ["subcategory.basic"] = "Temel",
+    ["subcategory.buttons"] = "Butonlar",
+    ["subcategory.characters"] = "Karakterler",
+    ["subcategory.colors"] = "Renkler",
+    ["subcategory.display"] = "Görüntü",
+    ["subcategory.distances"] = "Mesafeler",
+    ["subcategory.hud"] = "HUD",
+    ["subcategory.interaction"] = "Etkileşim",
+    ["subcategory.inventory"] = "Envanter",
+    ["subcategory.movement"] = "Hareket",
+    ["subcategory.position"] = "Pozisyon",
+    ["subcategory.size"] = "Boyut",
+
+    -- Config Translations
+    --- Chat
+    ---- Distances
+    ["config.chat.ic.distance"] = "IC Sohbet Mesafesi",
+    ["config.chat.me.distance"] = "ME Sohbet Mesafesi",
+    ["config.chat.ooc.distance"] = "OOC Sohbet Mesafesi",
+    ["config.chat.yell.distance"] = "YELL Sohbet Mesafesi",
+
+    --- Gameplay
+    ---- Interaction
+    ["config.hands.force.max"] = "Eller Maksimum El Kuvveti",
+    ["config.hands.force.max.throw"] = "Eller Maksimum Fırlatma Gücü",
+    ["config.hands.max.carry"] = "Eller Maksimum Taşıma Ağırlığı",
+    ["config.hands.range.max"] = "Eller Maksimum Uzanma Mesafesi",
+
+    ---- Inventory
+    ["config.inventory.weight.max"] = "Envanter Maksimum Ağırlık",
+
+    ---- Movement
+    ["config.jump.power"] = "Zıplama Gücü",
+    ["config.movement.bunnyhop.reduction"] = "Bunnyhop Hız Azaltma",
+    ["config.speed.run"] = "Koşma Hızı",
+    ["config.speed.walk"] = "Yürüme Hızı",
+    ["config.speed.walk.crouched"] = "Çömelme Yürüme Hızı",
+    ["config.speed.walk.slow"] = "Yavaş Yürüme Hızı",
+
+    --- General
+    ---- Basic
+    ["config.bot.support"] = "Bot Desteği",
+    ["config.language"] = "Dil",
+
+    ---- Characters
+    ["config.autosave.interval"] = "Karakteri Otomatik Kaydetme Sıklığı",
+    ["config.characters.max"] = "Maksimum Karakter Sayısı",
+
+    --- Interface
+    ["config.interface.font.antialias"] = "Yazı Tipi Kenar Yumuşatma",
+    ["config.interface.font.multiplier"] = "Yazı Tipi Ölçeği",
+
+    -- Options Translations
+    --- Chat
+    ---- Basic
+    ["option.chat.randomized.verbs"] = "Rastgele Sohbet Fiillerini Kullan",
+    ["chat.randomized.verbs.help"] = "Açık olduğunda, sohbet mesajları farklı fiiller kullanır (bağırır, homurdanır, haykırır). Kapalı olduğunda varsayılan fiilleri kullanır (der, fısıldar, bağırır).",
+
+    ---- Position
+    ["option.chatbox.x"] = "Sohbet Kutusu X Konumu",
+    ["option.chatbox.y"] = "Sohbet Kutusu Y Konumu",
+
+    ---- Size
+    ["option.chatbox.width"] = "Sohbet Kutusu Genişliği",
+    ["option.chatbox.height"] = "Sohbet Kutusu Yüksekliği",
+
+
+    --- Interface
+    ---- Buttons
+    ["option.button.delay.click"] = "Buton Tıklama Gecikmesi",
+
+    ---- Display
+    ["option.interface.scale"] = "Arayüz Ölçeği",
+    ["option.performance.animations"] = "Arayüz Animasyonlarını Etkinleştir",
+
+    ---- HUD
+    ["option.hud.bar.armor.show"] = "Zırh Barını Göster",
+    ["option.hud.bar.health.show"] = "Sağlık Barını Göster",
+    ["option.notification.enabled"] = "Bildirimleri Etkinleştir",
+    ["option.notification.length.default"] = "Varsayılan Bildirim Süresi",
+    ["option.notification.scale"] = "Bildirim Ölçeği",
+    ["option.notification.sounds"] = "Bildirim Seslerini Etkinleştir",
+
+    ---- Inventory
+    ["option.inventory.categories.italic"] = "Kategori Adlarını İtalik Yap",
+    ["option.inventory.columns"] = "Envanter Sütunlarının Sayısı",
+
+    -- Inventory Translations
+    ["inventory.weight.abbreviation"] = "kg",
+})
