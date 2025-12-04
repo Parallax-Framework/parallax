569c8e3267fec1ca98b612b59a3af3474b0cd237
Boz <40073828+Bozcx@users.noreply.github.com>
2025-12-04 06:00:24 +0000

    Create spawn perms (#76)

--- gamemode/modules/admin/core/sh_cami.lua
+++ gamemode/modules/admin/core/sh_cami.lua
@@ -2,3 +2,33 @@ CAMI.RegisterPrivilege({
     Name = "Parallax - Observer",
     MinAccess = "admin",
 })
+
+CAMI.RegisterPrivilege({
+    name = "Parallax - Spawn Props",
+    minaccess = "admin",
+})
+
+CAMI.RegisterPrivilege({
+    name = "Parallax - Spawn NPCs",
+    minaccess = "admin",
+})
+
+CAMI.RegisterPrivilege({
+    name = "Parallax - Spawn Weapons",
+    minaccess = "admin",
+})
+
+CAMI.RegisterPrivilege({
+    name = "Parallax - Spawn Entities",
+    minaccess = "admin",
+})
+
+CAMI.RegisterPrivilege({
+    name = "Parallax - Spawn Vehicles",
+    minaccess = "admin",
+})
+
+CAMI.RegisterPrivilege({
+    name = "Parallax - Spawn Ragdolls",
+    minaccess = "admin",
+})
