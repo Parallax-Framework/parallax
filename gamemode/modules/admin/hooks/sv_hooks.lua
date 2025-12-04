4903566ee58fa384daf8e593cf90a6be22a69183
Bozy <40073828+Bozcx@users.noreply.github.com>
2025-12-03 20:57:45 -0500

    Create spawn perms

--- gamemode/modules/admin/hooks/sv_hooks.lua
+++ gamemode/modules/admin/hooks/sv_hooks.lua
@@ -5,3 +5,60 @@ function MODULE:EntityTakeDamage(target, dmgInfo)
         return true
     end
 end
+
+function MODULE:PlayerSpawnEffect(client, model)CAM
+    if (!CAMI.PlayerHasAccess(client, "Parallax - Spawn Effects", nil)) then
+        return false
+    end
+
+    return true
+end
+
+function MODULE:PlayerSpawnNPC(client, model)
+    if (!CAMI.PlayerHasAccess(client, "Parallax - Spawn NPCs", nil)) then
+        return false
+    end
+
+    return true
+end
+
+function MODULE:PlayerSpawnProp(client, model)
+    if (!CAMI.PlayerHasAccess(client, "Parallax - Spawn Props", nil)) then
+        return false
+    end
+
+    return true
+end
+
+function MODULE:PlayerSpawnRagdoll(client, model)
+    if (!CAMI.PlayerHasAccess(client, "Parallax - Spawn Ragdolls", nil)) then
+        return false
+    end
+
+    return true
+end
+
+function MODULE:PlayerSpawnSENT(client, model)
+    if (!CAMI.PlayerHasAccess(client, "Parallax - Spawn SENTs", nil)) then
+        return false
+    end
+
+    return true
+end
+
+function MODULE:PlayerSpawnSWEP(client, model)
+    if (!CAMI.PlayerHasAccess(client, "Parallax - Spawn Weapons", nil)) then
+        return false
+    end
+
+    return true
+end
+
+function MODULE:PlayerSpawnVehicle(client, model)
+    if (!CAMI.PlayerHasAccess(client, "Parallax - Spawn Vehicles", nil)) then
+        return false
+    end
+    
+    return true
+end
+
