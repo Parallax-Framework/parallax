ac37ea8de9caecbc12d7588c58fff9502ab74804
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-12-24 23:51:07 +0200

    refactor(character): remove unused SetData method

--- gamemode/framework/meta/sh_character.lua
+++ gamemode/framework/meta/sh_character.lua
@@ -42,12 +42,6 @@ function character:GetData(key)
     return self.vars.data[key]
 end
 
-function character:SetData(key, value)
-    if ( !istable(self.vars.data) ) then self.vars.data = {} end
-
-    self.vars.data[key] = value
-end
-
 function character:GetOwner()
     return self.player
 end
