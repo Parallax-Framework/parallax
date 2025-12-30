96b07d64a10ae189497558dde8c12ffe5ca05e6d
Riggs <riggs9162@gmx.de>
2025-12-30 17:57:11 +0100

    feat(pac3): add PAC3 flag module for permission management

---
+++ gamemode/modules/sh_pac3.lua
@@ -0,0 +1,45 @@
+--[[
+    Parallax Framework
+    Copyright (c) 2025 Parallax Framework Contributors
+
+    This file is part of the Parallax Framework and is licensed under the MIT License.
+    You may use, copy, modify, merge, publish, distribute, and sublicense this file
+    under the terms of the LICENSE file included with this project.
+
+    Attribution is required. If you use or modify this file, you must retain this notice.
+]]
+
+local MODULE = MODULE
+
+MODULE.name = "PAC3 Flag"
+MODULE.description = "Adds a flag to PAC3."
+MODULE.author = "Riggs"
+
+function MODULE:PrePACConfigApply(client)
+    if ( client:GetCharacter() ) then
+        return client:GetCharacter():HasFlags("p")
+    end
+
+    return false
+end
+
+function MODULE:PrePACEditorOpen(client)
+    if ( client:GetCharacter() ) then
+        return client:GetCharacter():HasFlags("p")
+    end
+
+    return false
+end
+
+function MODULE:pac_CanWearParts(client)
+    if ( client:GetCharacter() ) then
+        return client:GetCharacter():HasFlags("p")
+    end
+
+    return false
+end
+
+ax.flag:Create("p", {
+    name = "PAC3 Permission",
+    description = "Allows the use of PAC3.",
+})
