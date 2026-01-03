7beadf8251e513953c6c714d2445051cd3c26f24
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2026-01-04 00:14:14 +0200

    Reapply "fix(inventory): improve receiver handling and validation"
    This reverts commit 77a2d623cccbc2d80fd6dd474f6b2a3807dc591e.

--- gamemode/framework/networking/cl_networking.lua
+++ gamemode/framework/networking/cl_networking.lua
@@ -361,7 +361,7 @@ net.Receive("ax.inventory.sync", function()
 end)
 
 net.Receive("ax.inventory.receiver.add", function()
-    local inventory = net.ReadTable()
+    local inventory = setmetatable(net.ReadTable(), ax.inventory.meta)
     local receiver = net.ReadPlayer()
 
     if ( !istable(inventory) ) then
@@ -384,6 +384,7 @@ net.Receive("ax.inventory.receiver.remove", function()
     end
 
     inventory:RemoveReceiver(receiver)
+    ax.inventory.instances[inventoryID] = nil
 end)
 
 net.Receive("ax.inventory.item.add", function()
@@ -494,7 +495,7 @@ net.Receive("ax.item.transfer", function()
     end
 
     -- Remove from the old inventory, if applicable
-    if ( fromInventoryID != 0 and fromInventory and fromInventory:HasReceiver(ax.client) ) then
+    if ( fromInventoryID != 0 and fromInventory and fromInventory:IsReceiver(ax.client) ) then
         fromInventory.items[item.id] = nil
     end
 
