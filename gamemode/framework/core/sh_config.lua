0ecd48cf5d66c6f891af9a1928e55f7c2adbe332
Riggs <riggs9162@gmx.de>
2026-03-03 21:23:35 +0100

    fix(config): set default language to empty string

--- gamemode/framework/core/sh_config.lua
+++ gamemode/framework/core/sh_config.lua
@@ -9,7 +9,7 @@
     Attribution is required. If you use or modify this file, you must retain this notice.
 ]]
 
-ax.config:Add("language", ax.type.string, "en", {
+ax.config:Add("language", ax.type.string, "", {
     description = "Server language",
     category = "general",
     subCategory = "basic"
