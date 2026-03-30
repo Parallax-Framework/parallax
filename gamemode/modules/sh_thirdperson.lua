6b64f450fb9d2d86d9b68fa258706953a33ec3b8
Riggs <riggs9162@gmx.de>
2026-03-30 04:05:38 +0200

    refactor(thirdperson): simplify head position retrieval logic

--- gamemode/modules/sh_thirdperson.lua
+++ gamemode/modules/sh_thirdperson.lua
@@ -267,24 +267,6 @@ function MODULE:ShouldDrawLocalPlayer(client)
     end
 end
 
-local function RetrieveHeadParameters(client)
-    if ( !IsValid(client) ) then return end
-
-    local headBone = client:LookupBone("ValveBiped.Bip01_Head1")
-    local eyeAttachment = client:LookupAttachment("eyes")
-
-    if ( headBone ) then
-        return client:GetBonePosition(headBone)
-    elseif ( eyeAttachment ) then
-        local eyeData = client:GetAttachment(eyeAttachment)
-        if ( eyeData and eyeData.Pos ) then
-            return eyeData.Pos, eyeData.Ang
-        end
-    end
-
-    return nil
-end
-
 ax.viewstack:RegisterModifier("thirdperson", function(client, patch)
     if ( hook.Run("ShouldUseThirdPerson", client) == false ) then return end
 
@@ -302,20 +284,18 @@ ax.viewstack:RegisterModifier("thirdperson", function(client, patch)
 
     -- start from the player's eye position
     local ragdoll = Entity(client:GetRelay("ragdoll.index", -1))
+    local bHasRagdoll = IsValid(ragdoll)
     local startPos = client:EyePos()
     local startAng = client:EyeAngles()
     if ( ax.option:Get("thirdperson.follow.head") ) then
-        local headPos, headAng = RetrieveHeadParameters(ragdoll or client)
+        local headEntity = bHasRagdoll and ragdoll or client
+        local headPos, headAng = ax.util:GetHeadTransform(headEntity)
         if ( headPos ) then
             startPos = headPos
         end
-
-        if ( headAng ) then
-            startAng = headAng
-        end
     else
-        if ( ragdoll ) then
-            startPos = RetrieveHeadParameters(ragdoll) or startPos
+        if ( bHasRagdoll ) then
+            startPos = ax.util:GetHeadTransform(ragdoll) or startPos
         end
     end
 
@@ -332,7 +312,7 @@ ax.viewstack:RegisterModifier("thirdperson", function(client, patch)
         filter = function(ent)
             if ( ent == client ) then return false end
             if ( ent:IsPlayer() and ent:Alive() ) then return false end
-            if ( IsValid(ragdoll) and ent == ragdoll ) then return false end
+            if ( bHasRagdoll and ent == ragdoll ) then return false end
             return true
         end,
         mask = MASK_SOLID
@@ -348,7 +328,7 @@ ax.viewstack:RegisterModifier("thirdperson", function(client, patch)
         filter = function(ent)
             if ( ent == client ) then return false end
             if ( ent:IsPlayer() and ent:Alive() ) then return false end
-            if ( IsValid(ragdoll) and ent == ragdoll ) then return false end
+            if ( bHasRagdoll and ent == ragdoll ) then return false end
             return true
         end,
         mask = MASK_SOLID
