0ea3afa00413592b628f96654f43567e58e68e46
bloodycop <bloodycop6385@gmail.com>
2026-04-02 01:18:46 +0300

    Update environment.lua

--- gamemode/framework/environment.lua
+++ gamemode/framework/environment.lua
@@ -1,8 +1,9 @@
 ax.ENV = ax.ENV or {}
 
-CreateConVar("ax_environment_name", "prod", {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "The current environment name")
-ax.ENV.name = GetConVar("ax_environment_name"):GetString()
+local ax_environment_name = CreateConVar("ax_environment_name", "prod", {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "The environment the server is running in")
+ax.ENV.name = ax_environment_name:GetString()
 
+cvars.RemoveChangeCallback("ax_environment_name", "ax_env_update")
 cvars.AddChangeCallback("ax_environment_name", function(convar, old, new)
     ax.ENV.name = new
     SetRelay("env_name", new)
