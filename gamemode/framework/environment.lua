39a6ddefb1ad1b89a7c3cf20c89e8bd22e89f551
bloodycop <bloodycop6385@gmail.com>
2026-04-03 18:44:43 +0300

    refactor: add a comment wohoo

--- gamemode/framework/environment.lua
+++ gamemode/framework/environment.lua
@@ -6,6 +6,8 @@ ax.ENV.name = ax_environment_name:GetString()
 cvars.RemoveChangeCallback("ax_environment_name", "ax_env_update")
 cvars.AddChangeCallback("ax_environment_name", function(convar, old, new)
     ax.ENV.name = new
+
+    -- TODO: Remove the bloody networking, IF and when https://github.com/Facepunch/garrysmod-issues/issues/3740 gets fixed
     SetRelay("env_name", new)
 end, "ax_env_update")
 
