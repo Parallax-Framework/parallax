145ad626d88646bb7b7c3c0110c209c6616ed4d1
bloodycop <bloodycop6385@gmail.com>
2026-04-03 19:38:06 +0300

    refactor: use BroadcastLua for environment convar, simplify ENV:IsDev Logic

--- gamemode/framework/environment.lua
+++ gamemode/framework/environment.lua
@@ -6,11 +6,9 @@ ax.ENV.name = ax_environment_name:GetString()
 cvars.RemoveChangeCallback("ax_environment_name", "ax_env_update")
 cvars.AddChangeCallback("ax_environment_name", function(convar, old, new)
     ax.ENV.name = new
-
-    -- TODO: Remove the bloody networking, IF and when https://github.com/Facepunch/garrysmod-issues/issues/3740 gets fixed
-    SetRelay("env_name", new)
+    BroadcastLua("ax.ENV.name = \"" .. new .. "\"")
 end, "ax_env_update")
 
 function ax.ENV:IsDev()
-    return SERVER and self.name != "prod" or GetRelay("env_name", "prod") != "prod"
+    return self.name != "prod"
 end
