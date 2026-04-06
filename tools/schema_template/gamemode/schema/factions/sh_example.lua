cd73d4612471cad1290eecec563260f213c1f6a1
Riggs <riggs9162@gmx.de>
2026-04-07 01:08:36 +0200

    feat(schema): add schema template initialization and related files
    introduces a new schema template for the Parallax Framework, including initialization scripts and example files for modules, items, languages, and configurations.

---
+++ tools/schema_template/gamemode/schema/factions/sh_example.lua
@@ -0,0 +1,22 @@
+--[[
+    Parallax Framework
+    Copyright (c) 2025-2026 Parallax Framework Contributors
+
+    This file is part of the Parallax Framework and is licensed under the MIT License.
+    You may use, copy, modify, merge, publish, distribute, and sublicense this file
+    under the terms of the LICENSE file included with this project.
+
+    Attribution is required. If you use or modify this file, you must retain this notice.
+]]
+
+FACTION = {}
+FACTION.name = "Example"
+FACTION.description = "An example faction."
+FACTION.color = Color(255, 255, 255)
+FACTION.isDefault = true
+
+FACTION.models = {
+    "models/player/Group01/male_07.mdl",
+}
+
+FACTION_EXAMPLE = FACTION.index
