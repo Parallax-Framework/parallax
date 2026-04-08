9e49846845df62646c25227e228a83048633a444
bloodycop <bloodycop6385@gmail.com>
2026-04-09 02:27:15 +0300

    feat: some door stuff

---
+++ gamemode/modules/doors/localization/sh_english.lua
@@ -0,0 +1,6 @@
+ax.localisation:Register("en", {
+    ["door"] = "Door",
+    ["door.access_group"] = "Door Access Group",
+
+    ["door.interface.access_groups"] = "Door Access Groups"
+})
--- gamemode/modules/doors/networking/sv_net.lua
+++ gamemode/modules/doors/networking/sv_net.lua
@@ -1,1 +1,25 @@
 local MODULE = MODULE
+
+ax.net:Hook("ax.doors.group_moveup", function(ply, groupEnum, groupIndex)
+    if ( !CAMI.PlayerHasAccess(ply, "Parallax - Doors - Manage Access Groups", nil) ) then return end
+    if ( groupEnum == "OWNER" ) then return end
+
+    local groups = MODULE.AccessGroups
+
+    print("moving", groupIndex, groupIndex - 1, groupEnum)
+
+    groups[groupEnum] = groupIndex - 1
+
+    PrintTable(groups)
+end, false)
+
+ax.net:Hook("ax.doors.group_movedown", function(ply, groupEnum, groupIndex)
+    if ( !CAMI.PlayerHasAccess(ply, "Parallax - Doors - Manage Access Groups", nil) ) then return end
+    if ( groupEnum == "OWNER" ) then return end
+
+    local groups = MODULE.AccessGroups
+
+    groups[groupEnum] = groupIndex + 1
+
+    PrintTable(groups)
+end, false)
