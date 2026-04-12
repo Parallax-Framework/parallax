0c449714176ad8dc2f64f6005f0f03c31048a5d2
Riggs <riggs9162@gmx.de>
2026-04-12 15:51:37 +0200

    refactor(thirdperson): use in-noclip method for view checks

--- gamemode/modules/sh_thirdperson.lua
+++ gamemode/modules/sh_thirdperson.lua
@@ -254,7 +254,7 @@ local curFOV
 
 -- LVS is gay and already used the ShouldDrawThirdPerson hook, so we have to use a different name
 function MODULE:ShouldUseThirdPerson(client)
-    if ( !client:Alive() or client:InVehicle() or client:GetObserverMode() != OBS_MODE_NONE or !client:Alive() or client:GetMoveType() == MOVETYPE_NOCLIP ) then return false end
+    if ( !client:Alive() or client:InVehicle() or client:GetObserverMode() != OBS_MODE_NONE or !client:Alive() or client:InNoclip() ) then return false end
 
     if ( !ax.config:Get("thirdperson") ) then return false end
 
